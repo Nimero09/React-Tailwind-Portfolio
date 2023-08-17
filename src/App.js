c0e3230cc7150a177b9df5fc4575b6c1f344f4c1
Nicolas Melo <jaimenicolasmelo@gmail.com>
2023-08-16 22:47:26 -0500

    Smooth scroll

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/about";
 import Skills from "./components/skills";
 import Work from "./components/work";
 import Contact from "./components/contact";
-
+ 
 function App() {
   return (
     <div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from "react-icons/fa"
-import { HiMail, HiOutlineMail } from "react-icons/hi";
+import { FaBars, FaTimes, FaGithub, FaLinkedin} from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo1.png";
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false); 
@@ -17,11 +18,31 @@ const Navbar = () => {
             {/* menu */}
             <div>
                 <ul className="hidden md:flex">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skills</li>
-                    <li>Work</li>
-                    <li>Contact</li>
+                    <li >
+                    <Link to="home" smooth={true} duration={500}>
+                    Home
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="about" smooth={true} duration={500}>
+                    About
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="skills" smooth={true} duration={500}>
+                    Skills
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="work" smooth={true} duration={500}>
+                    Work
+                    </Link>
+                    </li>
+                    <li>
+                    <Link to="contact" smooth={true} duration={500}>
+                    Contact
+                    </Link>
+                    </li>
                 </ul>
             </div>
 
@@ -31,27 +52,46 @@ const Navbar = () => {
             </div>
             {/* Mobile menu */}
             <ul className= {!nav ? "hidden" : "absolute top-0 left-0 w-full h-screen bg-[#08192f] flex flex-col justify-center items-center"}>
-                <li className="py-6 text-3xl">Home</li>
-                <li className="py-6 text-3xl">About</li>
-                <li className="py-6 text-3xl">Skills</li>
-                <li className="py-6 text-3xl">Work</li>
-                <li className="py-6 text-3xl">Contact</li>  
+                <li className="py-6 text-3xl">
+                    <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                    Home
+                    </Link>
+                </li>
+                <li className="py-6 text-3xl">
+                    <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                    About
+                    </Link>
+                </li>
+                <li className="py-6 text-3xl">
+                    <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                    Skills
+                    </Link>
+                </li>
+                <li className="py-6 text-3xl">
+                    <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                    Work
+                    </Link>
+                </li>
+                <li className="py-6 text-3xl">
+                    <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                    Contact
+                    </Link></li>  
             </ul>
 
             {/* social-icons */}
             <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
                 <ul>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
-                        <a className="flex justify-between items-center w-full text-gray" href="">LinkedIn <FaLinkedin size={30} /> </a>
+                        <a className="flex justify-between items-center w-full text-gray" href="https://www.linkedin.com/in/nicol%C3%A1s-melo-721080213/">LinkedIn <FaLinkedin size={30} /> </a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
-                        <a className="flex justify-between items-center w-full text-gray" href="">Github <FaGithub size={30} /> </a>
+                        <a className="flex justify-between items-center w-full text-gray" href="https://github.com/Nimero09">Github <FaGithub size={30} /> </a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
-                        <a className="flex justify-between items-center w-full text-gray" href="">Email <HiOutlineMail size={30} /> </a>
+                        <a className="flex justify-between items-center w-full text-gray" href="mailto: jaimenicolasmelo@gmail.com">Email <HiOutlineMail size={30} /> </a>
                     </li>
                     <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
-                        <a className="flex justify-between items-center w-full text-gray" href="">Resume <BsFillPersonLinesFill size={30} /> </a>
+                        <a className="flex justify-between items-center w-full text-gray" href="/">Resume <BsFillPersonLinesFill size={30} /> </a>
                     </li>
                 </ul>
             </div>
